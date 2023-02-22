bf78f529f2c4cb90d33a53bda1a55254afe88115
David Manzano <david.manzano@ceiba.com.co>
2023-02-22 09:22:45 -0500

    Se realiza ajuste de la versión del sdk en el pubspec.yaml

--- lib/user/repository/firebase_auth_api.dart
+++ lib/user/repository/firebase_auth_api.dart
@@ -6,15 +6,14 @@ class FirebaseAuthApi {
   final GoogleSignIn googleSignIn = GoogleSignIn();
 
   Future<UserCredential> signInWithGoogle() async {
-    final GoogleSignInAccount? googleSignInAccount =
-        await googleSignIn.signIn();
-    final GoogleSignInAuthentication? googleSignInAuthentication =
-        await googleSignInAccount?.authentication;
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
     final user = await _auth.signInWithCredential(
       GoogleAuthProvider.credential(
-        idToken: googleSignInAuthentication?.idToken,
-        accessToken: googleSignInAuthentication?.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken,
       ),
     );
     return user;
