6070ef2f2f764e0ce92c868a359d155785eb89da
David Manzano <david.manzano@ceiba.com.co>
2023-02-27 19:28:36 -0500

    Se muestra información del perfil desde firebase

---
+++ lib/user/model/user.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class User {
+  final String name;
+  final String email;
+  final String photoUrl;
+
+  //myFavoritePlaces
+  //myPlaces
+
+  User(this.name, this.email, this.photoUrl);
+
+}
--- lib/user/ui/screens/profile_header.dart
+++ lib/user/ui/screens/profile_header.dart
@@ -1,39 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/user/bloc/bloc_user.dart';
 import '../widgets/user_info.dart';
 import '../widgets/button_bar.dart';
+import '../../model/user.dart';
 
 class ProfileHeader extends StatelessWidget {
+  UserBloc userBloc;
+  User user;
+
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of<UserBloc>(context);
 
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-      ),
-    );
-
-    return Container(
-      margin: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 50.0
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              title
-            ],
-          ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return showProfileData(snapshot);
+          case ConnectionState.done:
+            return showProfileData(snapshot);
+          default:
+            return CircularProgressIndicator();
+        }
+      },
     );
   }
 
+  Widget showProfileData(AsyncSnapshot snapshot) {
+    if (!snapshot.hasData || snapshot.hasError) {
+      return Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 50.0),
+        child: Column(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text("No se pudo cargar la información"),
+          ],
+        ),
+      );
+    } else {
+      print(snapshot.data);
+      user = User(snapshot.data.displayName, snapshot.data.email, snapshot.data.photoURL);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0
+        ),
+      );
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                title
+              ],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );;
+    }
+  }
 }
